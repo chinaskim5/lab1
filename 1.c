d725f3e82e4f116839a9e0ff4b05140f2f6e6f90
Chinaski <emindanew@yandex.ru>
2017-02-19 22:56:55 +0800

    Task 1 no mistakes

--- 1.c
+++ 1.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 void init(int* arr, int n) {
-    arr = malloc(n * sizeof(int));
     int i;
     for (i = 0; i < n; ++i)
         arr[i] = i;
@@ -11,6 +10,7 @@ void init(int* arr, int n) {
 int main() {
     int* arr = NULL;
     int n = 10;
+    arr = malloc(n * sizeof(int));
     init(arr, n);
     int i;
     for (i = 0; i < n; ++i)
